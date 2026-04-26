8a35cf27f113e66e4e5aa81c00157b6d5678fbf1
x4tking-oss <x4t.king@gmail.com>
2026-04-26 11:59:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import { config } from 'dotenv';
 import { sequelize } from './core/Database.js';
 import { Bot } from './core/Bot.js';
-import http from 'http'; // <-- Ezt a sort hozzáadtuk
+import http from 'http';
+import fs from 'fs'; // <-- Fájlrendszer modul importálása
 
 config();
 
-// <-- Ezt a blokkot hozzáadtuk a webszerverhez -->
+// Létrehozzuk a hiányzó mappákat, ha nem léteznek a Renderen
+if (!fs.existsSync('./cache')) {
+    fs.mkdirSync('./cache');
+}
+if (!fs.existsSync('./logs')) {
+    fs.mkdirSync('./logs');
+}
+
+// Webszerver a Render Web Service miatt
 const port = process.env.PORT || 3000;
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -13,7 +22,6 @@ http.createServer((req, res) => {
 }).listen(port, () => {
     console.log(`Webszerver elindult a ${port}-es porton.`);
 });
-// <----------------------------------------------->
 
 (async () => {
     await sequelize.authenticate();
